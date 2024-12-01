9e839a38f3f7cf352a879cd659e8170ea915b6d3
darekszyper <114878453+darekszyper@users.noreply.github.com>
2024-12-01 20:14:12 +0100

    refactor

--- src/main/java/com/internship/juglottery/JugLotteryApplication.java
+++ src/main/java/com/internship/juglottery/JugLotteryApplication.java
@@ -9,16 +9,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class JugLotteryApplication {
 
-    @Value("${DATA_SOURCE_URL}")
-    private static String DATA_SOURCE_URL;
-
     public static void main(String[] args) {
         SpringApplication.run(JugLotteryApplication.class, args);
-        System.out.println("###############################");
-        System.out.println("###############################");
-        System.out.println("###############################");
-        System.out.println("###############################");
-        System.out.println(DATA_SOURCE_URL);
     }
 
 }
